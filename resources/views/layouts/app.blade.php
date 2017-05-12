48cf3b76ed5f8e99f2b054df3ed34684d2ebb759
Fil <fil.elman@greyandgreentech.com>
2017-05-12 14:59:26 +0800

    What I did is to render the correct login page for admin and user correctly when visiting the uri 'admin' or 'login'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,7 @@
 
 <body>
   <div id="app">
+    
     <nav class="navbar navbar-default navbar-static-top">
       <div class="container">
         <div class="navbar-header">
@@ -37,36 +38,36 @@
             <span class="icon-bar"></span>
           </button>
           <!-- Branding Image -->
-          <a class="navbar-brand" href="{{ url('/') }}"> {{ config('app.name', 'Laravel') }} </a> </div> 
-          <div class="collapse navbar-collapse" id="app-navbar-collapse"> 
-          <!-- Left Side Of Navbar --> 
-          <ul class="nav navbar-nav"> &nbsp; </ul> 
-          <!-- Right Side Of Navbar --> 
-          <ul class="nav navbar-nav navbar-right"> 
+          <a class="navbar-brand" href="{{ url('/') }}"> {{ config('app.name', 'Laravel') }} </a> </div>
+          <div class="collapse navbar-collapse" id="app-navbar-collapse">
+          <!-- Left Side Of Navbar -->
+          <ul class="nav navbar-nav"> &nbsp; </ul>
+          <!-- Right Side Of Navbar -->
+          <ul class="nav navbar-nav navbar-right">
             <!-- Authentication Links -->
-            @if (Auth::guest()) 
-              <li><a href="{{ route('login') }}">Login</a></li> 
-              <li><a href="{{ route('register') }}">Register</a></li> 
-            @else 
-              <li class="dropdown"> 
+            @if (Auth::guest())
+              <li><a href="{{ route('login') }}">Login</a></li>
+              <li><a href="{{ route('register') }}">Register</a></li>
+            @else
+              <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> {{ Auth::user()->name }} <span class="caret"></span> </a>
                 <ul class="dropdown-menu" role="menu">
-                  <li> 
+                  <li>
                     <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> Logout </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                      {{ csrf_field() }} 
+                      {{ csrf_field() }}
                     </form>
                   </li>
                 </ul>
-              </li> 
-            @endif 
+              </li>
+            @endif
           </ul>
         </div>
       </div>
-    </nav> 
-    
-    @yield('content') 
-    
+    </nav>
+
+    @yield('content')
+
   </div>
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}"></script>
