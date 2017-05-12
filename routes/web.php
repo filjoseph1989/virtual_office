85497dbe568eceacd7178bfcfc00d6699fdf22b6
Fil <fil@greyandgreentech.com>
2017-05-12 22:57:41 +0800

    Created two dashboard for user and admin

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::name('home')->get('/home', 'HomeController@index');
 
 /**
  * Route for admin dashboard, login form and login
